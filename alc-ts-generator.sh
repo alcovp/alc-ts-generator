cd82225b7592fbd6a75a6dd7ccf9943792973953
Vladimir Puchkovskiy <alcovp@gmail.com>
2024-07-20 23:12:01 +0200

    Initial commit

---
+++ alc-ts-generator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir $1
+cd $1
+yarn --yes init
+yarn add @types/node typescript 
+yarn add -D ts-node
+yarn tsc --init --rootDir src --outDir ./bin --esModuleInterop --lib ES2019 --module commonjs --noImplicitAny true
+mkdir src
+echo "console.log('Hello World\!\!\!')" > src/app.ts
+npx --yes json -I -f package.json -e "this.scripts={\"build\": \"tsc\",\"start\": \"node ./bin/app.js\",\"dev\": \"ts-node ./src/app.ts\"}"
